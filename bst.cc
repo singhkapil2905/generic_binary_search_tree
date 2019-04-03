c14ba35b7a52baa6f609a22ed742d2b7a90b6565
singhkapil2905 <k.devsinghnitw@gmail.com>
2019-04-04 02:34:02 +0530

    Create bst.cc
    This file shows example implementation using the bst.h source code file.

---
+++ bst.cc
@@ -0,0 +1,41 @@
+#include "bst.h"
+
+int main() {
+  kapil::BST<int> b;
+
+  int ch;
+  int el;
+
+  while (1) {
+    std::cout << "Enter ch : \n [1] Insert \n [2] Search \n [3] Delete \n [4] Show Tree \n";
+    std::cin >> ch;
+
+    switch (ch) {
+      case 1:
+        std::cout << "Enter the element to insert : \n";
+        std::cin >> el;
+        b.Insert(el);
+        break;
+      case 2:
+        std::cout << "Enter the element to be searched : \n";
+        std::cin >> el;
+        if (b.Search(el)) {
+          std::cout << "Element found \n";
+        } else {
+          std::cout << "Element not found \n";
+        }
+        break;
+      case 3:
+        std::cout << "Enter the elemet to be deleted : \n";
+        std::cin >> el;
+        b.Delete(el);
+        break;
+      case 4:
+        b.ShowTree();
+        break;
+      default:
+        return 0;
+    }
+  }
+  return 0;
+}
